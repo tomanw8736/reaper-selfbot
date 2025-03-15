b6aa4a275a1fdf148b6c3000b379ee3c4c513aa4
Thomas Anw <tomanw.dev@outlook.com>
2025-03-15 18:47:52 +1100

    Got the .userinfo [user] command working
    Returns an embed with information about the pinged user

--- main.py
+++ main.py
@@ -68,14 +68,38 @@ class Reaper:
             else:
                 await ctx.send(user + ' is trans!')
             return
-        
+       
+       # sends an embed with information on a pinged user
         @bot.command()
         async def userinfo(ctx, user=None):
             if not user:
                 print('[ERROR] No user specified!')
                 return
             else:
-                print(user.username)
+                userID = user.replace("@", "")
+                userID = userID.replace("<", "")
+                userID = userID.replace(">", "")
+                userObject = bot.get_user(userID)
+                print('[SUCCESS] Got user!')
+                avatar = userObject.internal_avatar
+                avatarID = avatar.id
+              #  print(avatarID)
+                avatarURL = "https://autumn.revolt.chat/avatars/" + avatarID + "?max_side=256"
+
+                # send userdata
+                userData = pyvolt.SendableEmbed(
+                    title="Reaper Selfbot v0.1.0 | Made by: tomanw#0380",
+                    description=f'''
+                    Display Name: {userObject.display_name}
+                    Username: {userObject.name}#{userObject.discriminator}
+                    User ID: {userObject.id}
+                    Online: {userObject.online}
+                    Bot: {userObject.bot}
+                    Avatar: {avatarURL}
+                    '''
+                )
+
+                await ctx.send(embeds=[userData])
         
 
 #        DO NOT UNCOMMENT - I AM NOT AT FAULT
@@ -96,8 +120,12 @@ class Reaper:
                 --**COMMANDS**--
                 ----------------------
                 - .help : Shows this message
+
                 - .penis : Sends a unicode penis of random length
                 - .aretheygay [user] : Runs a gay test (totally not rigged and totally not a miner)
+                - .aretheytrans [user] : Runs a trans test (totally not a bitcoin miner)
+
+                - .userinfo [user] : Send an embed with information about the user
                 ''', # lol wtf is this
             )
             print('[SUCCESS] Sent Help Embed')
